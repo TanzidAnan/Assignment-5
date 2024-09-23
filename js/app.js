3dcbba765d7a92ba1d8ea0b9b03ca3dad31d2c65
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-23 10:28:52 +0600

    funcation

--- js/app.js
+++ js/app.js
@@ -1,3 +1,44 @@
-const blogSection =document.getElementById('blog-section').addEventListener('click',function(){
-    window.location.href ='/blog.html'
+
+
+document.getElementById('blog-section').addEventListener('click', function () {
+    window.location.href = '/blog.html'
+})
+
+const niakhaliDonateBlance = document.getElementById('niakhaliDonateBlance');
+const totleBlanceEl = document.getElementById('totleBlance').innerText;
+console.log(totleBlanceEl.innerText)
+
+document.getElementById('niakhali-Donate-btn').addEventListener('click', function (e) {
+    e.preventDefault();
+    const niakaliInputFild = document.getElementById('niakhaliInput').value;
+    const niakhaliBlance = document.getElementById('niakhaliDonateBlance').innerText;
+
+    const niakhaliInputValue = parseFloat(niakaliInputFild);
+    const newNiakhaliBlance = parseFloat(niakhaliBlance);
+    const AllBlance = parseFloat(totleBlanceEl);
+
+
+
+    if (isNaN(niakhaliInputValue)) {
+        alert('Plice Enter Your Number')
+        return;
+    }
+    else {
+        const totleBlance = niakhaliInputValue + newNiakhaliBlance;
+        if (AllBlance < totleBlance) {
+            alert('Abable Blance');
+            return;
+        }
+        else {
+            alert(totleBlance + ' ' + 'Donate for Flood at Noakhali');
+            document.getElementById('niakhaliDonateBlance').innerText = totleBlance;
+            document.getElementById('totleBlance').innerText = AllBlance - totleBlance;
+
+            return;
+        }
+
+    }
+
+
+
 })
